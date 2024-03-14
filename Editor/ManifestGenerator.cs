df83986e7b1d14444bb92ea5bd58c65c32b38cc0
Andrey Lemin <andrey.lemin@softsmile.com>
2024-03-14 16:31:05 +0300

    Add OnPostprocessBuild step to ManifestGenerator
    # Conflicts:
    #	Editor/ManifestGenerator.cs

--- Editor/ManifestGenerator.cs
+++ Editor/ManifestGenerator.cs
@@ -9,34 +9,41 @@ using UnityEngine;
 
 namespace Elevator89.BuildPresetter
 {
-    public class ManifestGenerator : IPreprocessBuildWithReport
-    {
-        public int callbackOrder => 0;
-        
-        public static string ManifestFilePath => Path.Combine(StreamingAssetsPath, "manifest.txt");
-        private static string StreamingAssetsPath => Application.streamingAssetsPath;
+	public class ManifestGenerator : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+	{
+		public static string ManifestFilePath => Path.Combine(Application.streamingAssetsPath, "manifest.txt");
 
-        public void OnPreprocessBuild(BuildReport report)
-        {
-            if (report.summary.platform != BuildTarget.WebGL && report.summary.platform != BuildTarget.Android) 
-                return;
+		public int callbackOrder => 0;
 
-            if (File.Exists(ManifestFilePath))
-                File.Delete(ManifestFilePath);
+		public void OnPreprocessBuild(BuildReport report)
+		{
+			if (report.summary.platform != BuildTarget.WebGL && report.summary.platform != BuildTarget.Android)
+				return;
 
-            StringBuilder manifestContent = new StringBuilder();
-            IEnumerable<string> files = Directory.GetFiles(StreamingAssetsPath, "*", SearchOption.AllDirectories)
-                .Where(file => !file.EndsWith(".meta"));
+			StringBuilder manifestContent = new StringBuilder();
+			IEnumerable<string> files = Directory.GetFiles(Application.streamingAssetsPath, "*", SearchOption.AllDirectories)
+				.Where(file => !file.EndsWith(".meta"));
 
-            foreach (string file in files)
-            {
-                string relativePath = file.Substring(StreamingAssetsPath.Length + 1).Replace("\\", "/");
-                manifestContent.AppendLine(relativePath);
-            }
+			foreach (string file in files)
+			{
+				string relativePath = file.Substring(Application.streamingAssetsPath.Length + 1).Replace("\\", "/");
+				manifestContent.AppendLine(relativePath);
+			}
 
-            File.WriteAllText(ManifestFilePath, manifestContent.ToString());
-            AssetDatabase.Refresh();
-            Debug.Log($"Manifest generated at: {ManifestFilePath}");
-        }
-    }
+			File.WriteAllText(ManifestFilePath, manifestContent.ToString());
+			AssetDatabase.Refresh();
+			Debug.Log($"Manifest generated at: {ManifestFilePath}");
+		}
+
+		public void OnPostprocessBuild(BuildReport report)
+		{
+			if (report.summary.platform != BuildTarget.WebGL && report.summary.platform != BuildTarget.Android)
+				return;
+
+			if (File.Exists(ManifestFilePath))
+				File.Delete(ManifestFilePath);
+
+			AssetDatabase.Refresh();
+		}
+	}
 }
